c047d571d7470e05433169c4d8de14e6cd79443a
Neleac <caelenmensch@gmail.com>
2024-09-15 14:13:11 -0700

    face, pose, hands trackers

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,159 @@
-"use client";
+'use client';
 
-import { useEffect, useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
-import { Camera } from "@mediapipe/camera_utils";
-import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
-import { Holistic, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS } from "@mediapipe/holistic";
-import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
+import { Camera } from '@mediapipe/camera_utils';
+import { FaceLandmarker, PoseLandmarker, HandLandmarker, FilesetResolver, DrawingUtils } from '@mediapipe/tasks-vision';
+
+const DEVICE = 'GPU';
+const CAM_HEIGHT = 720, CAM_WIDTH = 1280;
 
 export default function Home() {
-    //const { nodes, materials } = useGLTF("https://models.readyplayer.me/622952275de1ae64c9ebe969.glb?morphTargets=ARKit");
-    const { nodes, materials } = useGLTF("/avatar.glb");
-    console.log(nodes);
-    const meshes = [nodes.EyeLeft, nodes.EyeRight, nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
+    // avatar
+    // const { nodes, materials } = useGLTF('https://models.readyplayer.me/622952275de1ae64c9ebe969.glb?morphTargets=ARKit');
+    // const { nodes, materials } = useGLTF('/avatar.glb');
+    // console.log(nodes);
+    // const meshes = [nodes.EyeLeft, nodes.EyeRight, nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
 
-    // face landmark model for blendshapes
-    let faceLandmarker;
-    async function createFaceLandmarker() {
-        const filesetResolver = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
-        faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+    // face, pose, hands landmark detection models
+    let faceTracker, poseTracker, handTracker;
+    async function createTrackers() {
+        const filesetResolver = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm');
+        
+        faceTracker = await FaceLandmarker.createFromOptions(filesetResolver, {
             baseOptions: {
-                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-                delegate: "GPU"
+                modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
+                delegate: DEVICE
             },
-            outputFaceBlendshapes: true,
-            runningMode: "VIDEO",
-            numFaces: 1
+            runningMode: 'VIDEO',
+            outputFaceBlendshapes: true
+        });
+
+        poseTracker = await PoseLandmarker.createFromOptions(filesetResolver, {
+            baseOptions: {
+                modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
+                delegate: DEVICE
+            },
+            runningMode: 'VIDEO'
+        });
+
+        handTracker = await HandLandmarker.createFromOptions(filesetResolver, {
+            baseOptions: {
+                modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
+                delegate: DEVICE
+            },
+            runningMode: 'VIDEO',
+            numHands: 2
         });
     }
-    createFaceLandmarker();
-
-    // holistic landmark model for body and hands
-    // const holistic = new Holistic({locateFile: (file) => {
-    //     return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
-    // }});
-    // holistic.setOptions({
-    //     modelComplexity: 1,
-    //     smoothLandmarks: true,
-    //     minDetectionConfidence: 0.5,
-    //     minTrackingConfidence: 0.5
-    // });
+    createTrackers();
 
     const video = useRef(null);
     const canvas = useRef(null);
     useEffect(() => {
-        // draw pose estimation results
-        // const canvasCtx = canvas.current.getContext('2d');
-        // holistic.onResults((result) => {
-        //     canvasCtx.save();
-        //     canvasCtx.clearRect(0, 0, canvas.current.width, canvas.current.height);
-        //     canvasCtx.drawImage(result.image, 0, 0, canvas.current.width, canvas.current.height);
-        //     drawConnectors(canvasCtx, result.poseLandmarks, POSE_CONNECTIONS, {color: '#00FF00', lineWidth: 4});
-        //     drawLandmarks(canvasCtx, result.poseLandmarks, {color: '#FF0000', lineWidth: 2});
-        //     drawConnectors(canvasCtx, result.faceLandmarks, FACEMESH_TESSELATION, {color: '#C0C0C070', lineWidth: 1});
-        //     drawConnectors(canvasCtx, result.leftHandLandmarks, HAND_CONNECTIONS, {color: '#CC0000', lineWidth: 5});
-        //     drawLandmarks(canvasCtx, result.leftHandLandmarks, {color: '#00FF00', lineWidth: 2});
-        //     drawConnectors(canvasCtx, result.rightHandLandmarks, HAND_CONNECTIONS, {color: '#00CC00', lineWidth: 5});
-        //     drawLandmarks(canvasCtx, result.rightHandLandmarks, {color: '#FF0000', lineWidth: 2});
-        //     canvasCtx.restore();
-        // });
-
+        const canvasCtx = canvas.current.getContext('2d');
+        const drawingUtils = new DrawingUtils(canvasCtx);
         let lastVideoTime = -1;
+
         const camera = new Camera(video.current, {
             onFrame: async () => {
-                if (faceLandmarker && lastVideoTime != video.current.currentTime) {
+                if (lastVideoTime != video.current.currentTime) {
                     lastVideoTime = video.current.currentTime;
 
-                    //await holistic.send({image: video.current});
+                    canvasCtx.save();
+                    canvasCtx.clearRect(0, 0, canvas.current.width, canvas.current.height);
 
-                    const faceResult = faceLandmarker.detectForVideo(video.current, performance.now());
-                    if (faceResult && faceResult.faceBlendshapes && faceResult.faceBlendshapes.length > 0)
-                    {
-                        const blendshapeResult = faceResult.faceBlendshapes[0].categories;
-                        blendshapeResult.forEach(result => {
-                            meshes.forEach(mesh => {
-                                const blenshapeIdx = mesh.morphTargetDictionary[result["categoryName"]];
-                                if (blenshapeIdx)
-                                {
-                                    mesh.morphTargetInfluences[blenshapeIdx] = result["score"];
+                    if (faceTracker) {
+                        const result = faceTracker.detectForVideo(video.current, performance.now());
+                        if (result) {
+                            if (result.faceLandmarks) {
+                                for (const landmarks of result.faceLandmarks) {
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_TESSELATION, { color: '#C0C0C070', lineWidth: 1 });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_EYE, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_FACE_OVAL, { color: '#E0E0E0' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS, { color: 'cyan' });
+                                    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS, { color: 'cyan' });
                                 }
-                            });
-                        });
+                            }
+
+                            // if (result.faceBlendshapes && result.faceBlendshapes.length > 0) {
+                            //     const blendshapes = result.faceBlendshapes[0].categories;
+                            //     blendshapes.forEach(blendshape => {
+                            //         meshes.forEach(mesh => {
+                            //             const blenshapeIdx = mesh.morphTargetDictionary[blendshape['categoryName']];
+                            //             if (blenshapeIdx) {
+                            //                 mesh.morphTargetInfluences[blenshapeIdx] = blendshape['score'];
+                            //             }
+                            //         });
+                            //     });
+                            // }
+                        }
                     }
+
+                    if (poseTracker) {
+                        const result = poseTracker.detectForVideo(video.current, performance.now());
+                        if (result) {
+                            for (const landmark of result.landmarks) {
+                                drawingUtils.drawLandmarks(landmark, {radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)});
+                                drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
+                            }
+                        }
+                    }
+
+                    if (handTracker) {
+                        const result = handTracker.detectForVideo(video.current, performance.now());
+                        if (result) {
+                            for (const landmark of result.landmarks) {
+                                drawingUtils.drawLandmarks(landmark, {radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1), color: 'green'});
+                                drawingUtils.drawConnectors(landmark, HandLandmarker.HAND_CONNECTIONS, { color: 'lime' });
+                            }
+                        }
+                    }
+
+                    canvasCtx.restore();
                 }
             },
-            width: 1280,
-            height: 720
+            width: CAM_WIDTH,
+            height: CAM_HEIGHT
         });
         camera.start();
 
         return function cleanup() {
             camera.stop();
-            //holistic.close();
-            if (faceLandmarker) {
-                faceLandmarker.close();
+            if (faceTracker) {
+                faceTracker.close();
+            }
+            if (poseTracker) {
+                poseTracker.close();
+            }
+            if (handTracker) {
+                handTracker.close();
             }
         };
     });
 
-    return (<>
-        <video ref={video} width="640px" height="360px"></video>
-        <canvas hidden ref={canvas} width="640px" height="360px"></canvas>
-        <div style={{ width: "50vw", height: "50vh" }}>
-            <Canvas>
-                <ambientLight intensity={0.1} />
-                <directionalLight position={[0, 0, 1]} />
-                <primitive object={nodes.Scene} position={[0, -17.5, 0]} scale={[10, 10, 10]} />
-            </Canvas>
+    return (
+        <div style={{ position: 'relative' }}>
+            <video ref={video} width={CAM_WIDTH} height={CAM_HEIGHT}></video>
+            <canvas ref={canvas} width={CAM_WIDTH} height={CAM_HEIGHT} style={{ position: 'absolute', top: 0, left: 0 }}></canvas>
         </div>
-    </>);
+    );
+
+    // return (<>
+    //     <video ref={video} width='640px' height='360px'></video>
+    //     <canvas hidden ref={canvas} width='640px' height='360px'></canvas>
+    //     <div style={{ width: '50vw', height: '50vh' }}>
+    //         <Canvas>
+    //             <ambientLight intensity={0.1} />
+    //             <directionalLight position={[0, 0, 1]} />
+    //             <primitive object={nodes.Scene} position={[0, -17.5, 0]} scale={[10, 10, 10]} />
+    //         </Canvas>
+    //     </div>
+    // </>);
 }
