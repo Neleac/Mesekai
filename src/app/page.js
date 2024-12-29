c523a1956adce5c4fc4d40014fca7a4920c9b15d
Neleac <caelenmensch@gmail.com>
2024-12-29 01:50:50 -0800

    avatar using readyplayerme url
    useGLTF only works in child component of Canvas, pass tracking results as props to Avatar component

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,10 @@
 import { Camera } from '@mediapipe/camera_utils';
 import { FaceLandmarker, PoseLandmarker, HandLandmarker, FilesetResolver, DrawingUtils } from '@mediapipe/tasks-vision';
 import { useEffect, useRef, useState } from 'react';
-import { useGLTF, OrbitControls } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 import Avatar from '@/components/avatar';
-import { animateBody, animateFace, animateHands, rotateHead } from '@/utils/solver';
 
 // hardware configuration
 const DEVICE = 'GPU';
@@ -16,7 +15,7 @@ const CAM_HEIGHT = 720;
 const CAM_WIDTH = 1280;
 
 let trackersCreated = false;
-let faceTracker, poseTracker, handTracker;
+let faceTracker, bodyTracker, handTracker;
 async function createTrackers() {
     const filesetResolver = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm');
     
@@ -30,42 +29,33 @@ async function createTrackers() {
         outputFacialTransformationMatrixes: true
     });
 
-    // poseTracker = await PoseLandmarker.createFromOptions(filesetResolver, {
-    //     baseOptions: {
-    //         modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
-    //         delegate: DEVICE
-    //     },
-    //     runningMode: MODE
-    // });
+    bodyTracker = await PoseLandmarker.createFromOptions(filesetResolver, {
+        baseOptions: {
+            modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
+            delegate: DEVICE
+        },
+        runningMode: MODE
+    });
 
-    // handTracker = await HandLandmarker.createFromOptions(filesetResolver, {
-    //     baseOptions: {
-    //         modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
-    //         delegate: DEVICE
-    //     },
-    //     runningMode: MODE,
-    //     numHands: 2,
-    //     min_hand_detection_confidence: 0.95,
-    //     min_hand_presence_confidence: 0.95
-    // });
-
-    trackersCreated = true;
+    handTracker = await HandLandmarker.createFromOptions(filesetResolver, {
+        baseOptions: {
+            modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
+            delegate: DEVICE
+        },
+        runningMode: MODE,
+        numHands: 2,
+        min_hand_detection_confidence: 0.95,
+        min_hand_presence_confidence: 0.95
+    });
 }
 
 export default function Home() {
-    // avatar
-    // const { nodes, _ } = useGLTF('/avatar.glb');
-    // const meshes = [nodes.EyeLeft, nodes.EyeRight, nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
-
-    if (!trackersCreated)
-    {
-        createTrackers();
-    }
+    const [faceTrackingResult, setFaceTrackingResult] = useState(null);
+    const [bodyTrackingResult, setBodyTrackingResult] = useState(null);
+    const [handTrackingResult, setHandTrackingResult] = useState(null);
 
     const video = useRef(null);
     const canvas = useRef(null);
-    const [nodes, setNodes] = useState(null);
-
     useEffect(() => {
         const canvasCtx = canvas.current.getContext('2d');
         const drawingUtils = new DrawingUtils(canvasCtx);
@@ -74,6 +64,11 @@ export default function Home() {
         
         const camera = new Camera(video.current, {
             onFrame: async () => {
+                if (!trackersCreated) {
+                    await createTrackers();
+                    trackersCreated = true;
+                }
+
                 if (lastVideoTime != video.current.currentTime) {
                     lastVideoTime = video.current.currentTime;
 
@@ -83,6 +78,8 @@ export default function Home() {
                     if (faceTracker) {
                         trackingResult = faceTracker.detectForVideo(video.current, performance.now());
                         if (trackingResult) {
+                            setFaceTrackingResult(trackingResult);
+
                             // video overlay
                             if (trackingResult.faceLandmarks) {
                                 for (const landmarks of trackingResult.faceLandmarks) {
@@ -97,31 +94,18 @@ export default function Home() {
                                     drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS, { color: 'cyan', lineWidth: CAM_HEIGHT / 1000 });
                                 }
                             }
-
-                            // if (trackingResult.faceBlendshapes && trackingResult.faceBlendshapes.length > 0) {
-                            //     const meshes = [nodes.EyeLeft, nodes.EyeRight, nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
-                            //     animateFace(meshes, trackingResult.faceBlendshapes[0].categories);
-                            // }
-
-                            console.log(nodes);
-
-                            if (trackingResult.facialTransformationMatrixes && trackingResult.facialTransformationMatrixes.length > 0) {
-                                rotateHead(nodes, trackingResult.facialTransformationMatrixes[0].data)
-                            }
                         }
                     }
 
-                    if (poseTracker) {
-                        trackingResult = poseTracker.detectForVideo(video.current, performance.now());
+                    if (bodyTracker) {
+                        trackingResult = bodyTracker.detectForVideo(video.current, performance.now());
                         if (trackingResult) {
+                            setBodyTrackingResult(trackingResult);
+
                             // video overlay
                             for (const landmark of trackingResult.landmarks) {
                                 drawingUtils.drawLandmarks(landmark, {radius: CAM_HEIGHT / 1000});
                                 drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS, { color: 'lime', lineWidth: CAM_HEIGHT / 500 });
-                            }
-
-                            if (trackingResult.worldLandmarks && trackingResult.worldLandmarks.length > 0) {
-                                animateBody(nodes, trackingResult.worldLandmarks[0]);
                             }
                         }
                     }
@@ -129,13 +113,13 @@ export default function Home() {
                     if (handTracker) {
                         trackingResult = handTracker.detectForVideo(video.current, performance.now());
                         if (trackingResult) {
+                            setHandTrackingResult(trackingResult);
+
                             // video overlay
                             for (const landmark of trackingResult.landmarks) {
                                 drawingUtils.drawLandmarks(landmark, {color: 'green', radius: CAM_HEIGHT / 1000 });
                                 drawingUtils.drawConnectors(landmark, HandLandmarker.HAND_CONNECTIONS, { color: 'lime', lineWidth: CAM_HEIGHT / 1000 });
                             }
-
-                            animateHands(nodes, trackingResult);
                         }
                     }
 
@@ -152,14 +136,14 @@ export default function Home() {
             if (faceTracker) {
                 faceTracker.close();
             }
-            if (poseTracker) {
-                poseTracker.close();
+            if (bodyTracker) {
+                bodyTracker.close();
             }
             if (handTracker) {
                 handTracker.close();
             }
         };
-    }, [nodes]);
+    }, []);
 
     return (
         <div style={{ display: 'flex' }}>
@@ -171,9 +155,12 @@ export default function Home() {
                 <color attach='background' args={['grey']} />
                 <ambientLight intensity={0.1} />
                 <directionalLight position={[0, 0, 1]} />
-                <Avatar setNodes={setNodes} />
+                <Avatar 
+                    userFace={faceTrackingResult} 
+                    userBody={bodyTrackingResult}
+                    userHands={handTrackingResult}
+                />
                 <OrbitControls />
-                {/* <primitive object={nodes.Scene} position={[0, -1, 3.5]} /> */}
             </Canvas>
         </div>
     );
--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,9 +1,30 @@
 import { useGLTF } from '@react-three/drei';
 
-export default function Avatar({ setNodes }) {
-    const { nodes, _ } = useGLTF('https://models.readyplayer.me/622952275de1ae64c9ebe969.glb?morphTargets=ARKit');
-    setNodes(nodes);
+import { animateBody, animateFace, animateHands, rotateHead } from '@/utils/solver';
 
+
+export default function Avatar({ userFace, userBody, userHands }) {
+    const { nodes, _ } = useGLTF('https://models.readyplayer.me/622952275de1ae64c9ebe969.glb?morphTargets=ARKit');
+    const meshes = [nodes.EyeLeft, nodes.EyeRight, nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
+
+    if (userFace) {
+        if (userFace.faceBlendshapes && userFace.faceBlendshapes.length > 0) {
+            animateFace(meshes, userFace.faceBlendshapes[0].categories);
+        }
+
+        if (userFace.facialTransformationMatrixes && userFace.facialTransformationMatrixes.length > 0) {
+            rotateHead(nodes, userFace.facialTransformationMatrixes[0].data)
+        }
+    }
+
+    if (userBody && userBody.worldLandmarks && userBody.worldLandmarks.length > 0) {
+        animateBody(nodes, userBody.worldLandmarks[0]);
+    }
+
+    if (userHands) {
+        animateHands(nodes, userHands);
+    }
+    
     return (
         <primitive object={nodes.Scene} position={[0, -1, 3.5]} />
     );
